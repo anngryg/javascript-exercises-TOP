5a55fc091d2325ece568a0dfbaca170f78edd252
Anna <anna.grygierczyk@wp.pl>
2023-08-22 13:49:54 +0200

    fibonacci'

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num===0){
+        return 0;
+    }else if (num<0){
+        return error;
+    }else{
+        let firstPrev = 1;
+        let secondPrev = 0;
+        for (let i=2; i<=num; i++){
+            let current = firstPrev + secondPrev;
+            secondPrev=firstPrev;
+            firstPrev=current;
+        }
+        return firstPrev
+    }
+    
 };
-
+console.log(fibonacci(8))
 // Do not edit below this line
 module.exports = fibonacci;
