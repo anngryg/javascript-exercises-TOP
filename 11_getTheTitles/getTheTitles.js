b49d0d7c77530c65c43e10276e2d8dfba3af6cda
Anna <anna.grygierczyk@wp.pl>
2023-08-22 14:16:15 +0200

    get the titles done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
 
+const getTheTitles = function(arr) {
+    let result = arr.map(book=> book.title);
+    return result
 };
-
+console.log(getTheTitles(books));
 // Do not edit below this line
 module.exports = getTheTitles;
