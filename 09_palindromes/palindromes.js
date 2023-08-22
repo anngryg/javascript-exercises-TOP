0e417e5e9a8bbe0c1e9d806865ed35da38e9a7ae
Anna <anna.grygierczyk@wp.pl>
2023-08-22 12:58:59 +0200

    09_palindroned added

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function () {
+const palindromes = function (input) {
+       let lowerCase = input.toLowerCase();
+       let replace = lowerCase.replace(/[^a-z0-9]/g, "")
+       let split = replace.split('');
+       let reverse = split.reverse();
+       let join = reverse.join('');
+       if(join===replace){
+        return true;
+       }else{
+        return false;
+       }
 
 };
 
