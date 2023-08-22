420073b5be904ffe006d375e57a083cc30a33ec2
Anna <anna.grygierczyk@wp.pl>
2023-08-22 11:49:36 +0200

    calculator task filled

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sumArr = arr.reduce((sum,current) => sum+current, 0);
+  return sumArr;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	let multiplyNum = arr.reduce((sum,current) => sum*current);
+  return multiplyNum;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+let output=1;
+for (i=0;i<b;i++){
+  output=output*a;
+}
+return output
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let output=1;
+	for (i=a; i>0; i--){
+    output=output*i;
+  }
+  return output;
 };
 
 // Do not edit below this line
