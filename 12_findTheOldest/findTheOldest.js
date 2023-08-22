14c58ddf674b551da0f458e0cf8b8a4753c60706
Anna <anna.grygierczyk@wp.pl>
2023-08-22 15:04:41 +0200

    find oldest person finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
 
+const findTheOldest = function(arr) {
+    let sort = arr.sort(function(a,b){
+    liveda = a.passed - a.year;
+    livedb = b.passed - b.year;
+
+    if(liveda>livedb){
+        return 1;
+    }else{
+        return -1;
+    }
+})
+return sort.pop()
 };
 
 // Do not edit below this line
